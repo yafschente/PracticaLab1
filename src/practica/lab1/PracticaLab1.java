7cc303ff5eaeeb687b6a9c6a2f01faec88d7014f
carlos cerrato <carlos cerrato@LAPTOP-CJVIVBNS>
2022-04-22 15:15:52 -0600

    El anexo de la clase

---
+++ src/practica/lab1/PracticaLab1.java
@@ -0,0 +1,10 @@
+package practica.lab1;
+
+public class PracticaLab1 {
+    public static void main(String[] args) {
+        int i = 1234;
+        System.out.println("Hola mundo! "+"\nValor numerico"+(1+12.34)+"$ ");
+        System.out.println("Datos de alumno");
+    }
+    
+}
